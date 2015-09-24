781b7d260a57b853f55ad2c36c68426e406600ed
Manjesh V <manjesh.vinayaka@florahospitality.com>
2015-09-24 12:14:14 +0530

    made app/category as default route

--- www/app/app.js
+++ www/app/app.js
@@ -60,7 +60,7 @@
         })
 
       // if none of the above states are matched, use this as the fallback
-      $urlRouterProvider.otherwise('/app');
+      $urlRouterProvider.otherwise('/app/category');
       //$locationProvider.html5Mode(true);
     })
 
