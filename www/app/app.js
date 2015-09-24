67d173b5c021fdd3cf3a13ba221522d80933d824
Manjesh V <manjesh.vinayaka@florahospitality.com>
2015-09-24 17:16:18 +0530

    http://localhost:8100/#/app/products/1 DB and DBProduct factory added. and Db working fine. Need to link webservice into DB

--- www/app/app.js
+++ www/app/app.js
@@ -5,18 +5,25 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 
+// Add this file before all other custom js files
+var db;
 
 (function() {
   'use strict';
 
   angular.module('starter', ['ionic'])
 
-    .run(function($ionicPlatform) {
+    .run(function($ionicPlatform,DB) {
       console.log('run')
+
+
       $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         console.log('ionic ready')
+
+        DB.init();
+
         if (window.cordova && window.cordova.plugins.Keyboard) {
           cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
           cordova.plugins.Keyboard.disableScroll(true);
@@ -68,6 +75,20 @@
     .constant("APP_CONFIG", {
       baseApiUrl: "http://android.par-ken.com/healthyalsi"
     })
+    .constant('DB_CONFIG', {
+      name: 'DB',
+      tables: [
+        {
+          name: 'products',
+          columns: [
+            {name: 'id', type: 'integer'},
+            {name: 'categoryName', type: 'text'},
+            {name: 'description', type: 'text'},
+            {name: 'image', type: 'text'}
+          ]
+        }
+      ]
+    });
 
   'use strict';
   // Global Controller or Parent Controller
@@ -122,6 +143,125 @@
 
 
   });
+
+  function populateDB(tx) {
+		/*
+		 * Service: GET_RMF_NEWS
+		 * @params NEWS_FLAG 1|2|3
+		 */
+		tx.executeSql('DROP TABLE IF EXISTS rmf_news');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS rmf_news (news_id INTEGER,news_header TEXT,news_content TEXT, news_url TEXT, datetime TEXT, is_enabled INTEGER, news_type INTEGER)');
+
+		/*
+		 * NFO_LIST
+		 * @params
+		 */
+		tx.executeSql('DROP TABLE IF EXISTS nfo_product_calender');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS nfo_product_calender (nfo_id INTEGER,nfo_name TEXT, nfo_start_date TEXT, nfo_end_date TEXT, maturity_period TEXT)');
+
+		/*
+		 * VIDEOS
+		 * @params VIDEO_FLAG 0|1|2
+		 */
+
+		tx.executeSql('DROP TABLE IF EXISTS rmf_videos')
+		tx.executeSql('CREATE TABLE IF NOT EXISTS rmf_videos (video_id INTEGER,video_header TEXT, cover_image TEXT, video_expiry_date TEXT, video_duration TEXT,video_youtube_url TEXT,video_download_url TEXT,video_date  TEXT)');
+
+		/*
+		 * EQUITY_QUANTS
+		 *  @params
+		 */
+
+		tx.executeSql('DROP TABLE IF EXISTS rmf_equity_quants');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS rmf_equity_quants (scheme_id INTEGER,scheme_name TEXT,aum INTEGER, weighted_avg_market_cap TEXT, large_cap TEXT, mid_cap TEXT, small_cap TEXT upload_datetime TEXT, quants_doc_url TEXT,doc_size TEXT, is_enabled INTEGER)');
+
+		/*
+		 * DEBT_QUANTS
+		 * @params
+		 */
+
+		tx.executeSql('DROP TABLE IF EXISTS rmf_debt_quants');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS rmf_debt_quants (scheme_id INTEGER,scheme_name TEXT,aum INTEGER, ytm TEXT, duration TEXT,wt_average_maturity TEXT, quants_doc_url TEXT, doc_size TEXT)');
+
+		/*
+		 * GET_RMF_NEWS_HOME
+		 * @params
+		 */
+
+		tx.executeSql('DROP TABLE IF EXISTS rmf_news_home');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS rmf_news_home (news_id INTEGER,news_header TEXT,news_content TEXT, news_url TEXT, datetime TEXT, is_enabled INTEGER, news_type INTEGER)');
+
+		/*
+		 * NFO_PRODUCT_CALENDER_HOME
+		 * $params
+		 */
+
+		tx.executeSql('DROP TABLE IF EXISTS nfo_product_calender_home');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS nfo_product_calender_home (nfo_home_id INTEGER,nfo_name TEXT, nfo_start_date TEXT, nfo_end_date TEXT,maturity_period TEXT, is_enabled INTEGER)');
+
+		/*
+		 * MARKETS_AND_ECONOMY
+		 *
+		 */
+
+		tx.executeSql('DROP TABLE IF EXISTS market_economy');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS market_economy (me_id INTEGER, sub_type TEXT,me_header TEXT, me_content TEXT, doc_url TEXT,doc_size TEXT)');
+
+		/*
+		 * RMF_DIFFERENCE
+		 *
+		 */
+
+		tx.executeSql('DROP TABLE IF EXISTS rmf_difference');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS rmf_difference (rmf_id INTEGER, rmf_header TEXT,rmf_content TEXT, doc_url TEXT,doc_size TEXT,lead_generation TEXT)');
+
+		/*
+		 * Download Documents Local File System
+		 *
+		 */
+
+		tx.executeSql('DROP TABLE IF EXISTS rmf_local_documents');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS rmf_local_documents (doc_name TEXT,doc_size  TEXT, doc_url  TEXT,doc_local_url  TEXT,doc_type TEXT)');
+
+		/*
+		 * Downloaded Videos Local File System
+		 */
+
+		tx.executeSql('DROP TABLE IF EXISTS rmf_download_videos');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS rmf_download_videos (video_id INTEGER,video_header text,video_duration text,video_date text,video_youtube_url text,video_local_url text)');
+
+		tx.executeSql('DROP TABLE IF EXISTS nfo_documents');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS nfo_documents (nfo_id INTEGER,nfo_doc_type TEXT, nfo_doc_url TEXT, nfo_doc_name TEXT)');
+
+
+		/*
+		 * FUND_PERFORMANCE
+		 *
+		 */
+		tx.executeSql('DROP TABLE IF EXISTS rmf_equity_funds');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS rmf_equity_funds (news_id, news_header, news_content, news_url, datetime, is_enabled, news_type)');
+
+		/*
+		 * Messages
+		 *
+		 */
+
+		tx.executeSql('DROP TABLE IF EXISTS rmf_messages1');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS rmf_messages1 (msg_id PRIMARY KEY,msg_content TEXT,doc_url TEXT,doc_size TEXT,uploaddate TEXT,read_flag INTEGER)');
+
+
+
+	}
+
+	function errorCB() {
+		alert("Error: Init.js ErrorCB");
+	}
+
+	function successCB() {
+		//alert();
+		console.log('DB Created Success');
+	}
+
 
 }).call(this);
 
