465506fe6611295208bc4a5a6dd2794005f1e4a8
Manjesh V <manjesh.vinayaka@florahospitality.com>
2015-10-09 12:50:31 +0530

    Localstorage factory

--- www/app/factories/LS/LS.js
+++ www/app/factories/LS/LS.js
@@ -5,25 +5,46 @@
   angular.module('starter').factory('LS',  function($window) {
     return {
       set: function(key, value) {
-        $window.localStorage[key] = value;
+        localStorage[key] = value;
       },
       get: function(key, defaultValue) {
-        return $window.localStorage[key] || false;
+        return localStorage[key] || false;
       },
       setObject: function(key, value) {
-        $window.localStorage[key] = JSON.stringify(value);
+        localStorage[key] = JSON.stringify(value);
       },
       getObject: function(key) {
-        if($window.localStorage[key] != undefined)
-          return JSON.parse($window.localStorage[key] || false );
+        if(localStorage[key] != undefined)
+          return JSON.parse(localStorage[key] || false );
 
         return false;
       },
+      push:function(key,value){
+        var data;
+        if(localStorage[key] != undefined){
+          data = JSON.parse(localStorage[key])
+        } else {
+          data = []
+        }
+        data.push(value)
+        localStorage[key] = JSON.stringify(data);
+      },
+      pop:function (key,objectKey,objectValue){
+        if(localStorage[key] != undefined) {
+          var data = JSON.parse(localStorage[key])
+          for(var i = 0; i < data.length; i++) {
+            if(data[i][objectKey] == objectValue) {
+              data.splice(i, 1);localStorage[key]= JSON.stringify(data);
+              break;
+            }
+          }
+        }
+      },
       remove: function(key){
-        $window.localStorage.removeItem(key);
+        localStorage.removeItem(key);
       },
       clear: function(){
-        $window.localStorage.clear();
+        localStorage.clear();
       }
     }
   })
